b38e45081a28ac4a7f811f1ed280207313f815ad
Anjali Kashyap <anjali.kashyap93@gmail.com>
2021-09-11 17:48:56 +0530

    added all the files

---
+++ Model.py
@@ -0,0 +1,52 @@
+# import libraties
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, HashingVectorizer
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+
+
+lr_pickle_model = joblib.load('./models/lr_model.pkl') 
+user_final_rating = pd.read_pickle('./models/final_user_rating.pkl')
+products_details = pd.read_csv('sample30.csv')
+tfidf_vector = TfidfVectorizer(stop_words="english")
+
+def RecommendProducts(user_input):
+# This is the function where we use the recommendation system with the sentiment analysis model and get the top 5 products for recommendation 
+    pro_df = user_final_rating.loc[user_input]
+    pro_df = pro_df.reset_index()
+    pro_df = pro_df.sort_values(by=user_input,ascending=False)[0:20]
+    
+
+    pro_reviews=dict()
+    pro_review_per=dict()
+    pro_df = pro_df.reset_index(drop = True)
+    
+    for pro_name in pro_df['index']:
+        print('pro_name', pro_name)
+        review_test = products_details[products_details['name']==pro_name].reviews_text
+        pro_reviews[pro_name] = lr_pickle_model.predict(review_test)
+        (unique, counts) = np.unique(pro_reviews[pro_name], return_counts=True)
+        
+        frequencies = np.asarray((unique, counts)).T
+        
+        if len(frequencies)==2: ### get the counts for positive and negative reviews
+            pos_rev=frequencies[1][1]
+            neg_rev=frequencies[0][1]
+        else:
+            if frequencies[0][0]=='Positive':
+                pos_rev=frequencies[0][1]
+                neg_rev=0
+            else:
+                pos_rev=0
+                neg_rev=frequencies[0][1]
+        total= pos_rev+neg_rev
+        pro_review_per[pro_name] = pos_rev/total  
+
+    pro_review_per_sorted=sorted(pro_review_per.items(), key=lambda x: x[1], reverse=True)
+    pro_review_per_sorted=list(zip(*pro_review_per_sorted))
+    output=pd.DataFrame(list(pro_review_per_sorted[0])[:5]) #sort and get the 5 products
+    return output
+
+
--- User_Recommendation_System.py
+++
@@ -1,8 +0,0 @@
-# import libraties
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-review_recommend_user = review_recommend_user[~review_recommend_user['reviews_username'].isna()]
-
-
